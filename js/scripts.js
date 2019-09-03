aa2f2a2dca6226f868094b2d20341963c8d162a0
Stefano Di Pierro <stefanodipierro@live.com>
2019-09-03 18:40:09 +0200

    Npm Script 1

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+        $(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 2000 } );
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+                
+            
+          
+        });
+        $(document).ready(function(){
+            $("#reserveButton").click(function(){
+               $("#reservationDiv").modal('show');
+            });
+        });
+        $(document).ready(function(){
+            $("#loginBtn").click(function(){
+               $("#loginModal").modal('show');
+            });
+        });
